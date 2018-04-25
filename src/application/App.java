ccf996ee98a74ba3e81b6cf50a6a6855afdb0ec5
BaronVladziu <baronvladziu@gmail.com>
2018-04-25 10:10:49 +0200

    Displaying window

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,17 @@
 package application;
 
+import java.awt.*;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Window();
+            }
+        });
     }
 
 }
---
+++ src/application/Window.java
@@ -0,0 +1,13 @@
+package application;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+    public Window() {
+        super("Project-FEM");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
